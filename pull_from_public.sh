ff9a9d8dd25134d76e7049b16cfb87a9d9ee91f4
Andrés Mejía <andmej@gmail.com>
2022-04-26 11:35:39 -0500

    Remove unused scripts.

--- pull_from_public.sh
+++
@@ -1,8 +0,0 @@
-branch=$(git branch --show-current)
-ssh github-bridge /bin/bash << EOF
-  cd ~/code/github.com/lh265/terra_example_contracts
-  git fetch public
-  git checkout public/$branch
-  git push internal HEAD:$branch
-EOF
-git pull internal $branch
--- push_to_public.sh
+++
@@ -1,8 +0,0 @@
-branch=$(git branch --show-current)
-git push internal $branch
-ssh github-bridge /bin/bash << EOF
-  cd ~/code/github.com/lh265/terra_example_contracts
-  git fetch internal
-  git checkout internal/$branch
-  git push public HEAD:$branch
-EOF
