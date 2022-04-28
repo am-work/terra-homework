9eadf5c1739ce74729a582fbdd07717d429433b6
Andrés Mejía <andmej@gmail.com>
2022-04-28 17:52:23 -0500

    Add comments to scripts/swap2/execute-buy.js

--- scripts/swap2/execute-buy.js
+++ scripts/swap2/execute-buy.js
@@ -5,18 +5,23 @@ import {
   Coins,
 } from "@terra-money/terra.js";
 
+// Address of the Lemon Swap contract.
 const contract = "terra1f7vnhhk24jmaxp2tdgf906yy980yg3c4h8f05h";
+// Wallet to use. Make sure to use the right wallet from library.js.
 const wallet = wallets.wallet1;
 
-
-const amount = (0.5 * 1e6).toFixed(0);
+const amount = (0.5 * 1e6).toFixed(0); // 0.5 Luna
 
 const msg = new MsgExecuteContract(
+  // Address of person who's signing the transaction.
   wallet.key.accAddress,
+  // Address of contract to execute.
   contract,
+  // ExecuteMsg payload
   {
     buy: {},
   },
+  // Send Luna with this execute message.
   new Coins({ uluna: amount }),
 );
 
