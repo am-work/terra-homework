abc4c5470af7640fa340363b228cd723fec7349d
Andrés Mejía <andmej@gmail.com>
2022-05-02 14:29:38 -0500

    Add comments to scripts/cw20/query-balance.js.

--- scripts/cw20/query-balance.js
+++ scripts/cw20/query-balance.js
@@ -10,7 +10,16 @@ import {
 const cw20Contract = "terra1hpajld8zs93md8zrs6sfy42zl0khqpmr07muw0";
 const walletAddress = wallets.wallet1.key.accAddress;
 
-const response = await client.wasm.contractQuery(cw20Contract, { balance: { address: walletAddress }});
+const response = await client.wasm.contractQuery(
+  // Address of CW20 contract.
+  cw20Contract,
+  // QueryMsg payload.
+  {
+    balance: {
+      address: walletAddress
+    }
+  }
+);
 
 console.log(response);
 
